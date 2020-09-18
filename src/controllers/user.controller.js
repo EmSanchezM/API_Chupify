6adae2c5db346403e92d0bcb7e49d09057d381f3
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-09-18 16:53:53 -0600

    crud users completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,16 +8,19 @@ const { generarJWT } = require('../helpers/jwt');
 
 const userController = {};
 
+//Obtener todos los usuarios
 userController.getUsers = async(req, res)=>{
     const users = await User.find().populate('role');
     return res.json(users);
 }
 
+//Obtener usuario por ID: user_id
 userController.getUserById = async(req, res)=>{
     const user = await User.findById(req.params.user_id).populate('role')
     return res.status(200).json(user);
 }
 
+//Crear un usuario
 userController.createUser = async(req, res=response)=>{
     //Necesito el email para verificar si existe y el password para encriptarlo
     const {first_name, last_name, email, password, role} = req.body;
@@ -61,6 +64,73 @@ userController.createUser = async(req, res=response)=>{
             ok:false,
             message: 'Error inesperado..revisa logs'
         })
+    }
+}
+
+//Actualizar un usuario
+userController.updateUser = async(req, res)=>{
+    const id = req.params.user_id;
+    try {
+        const userUpdate = User.findById(id);
+
+        if(!userUpdate){
+            return res.status(404).json({
+                ok:false,
+                message: 'No existe usuario con ese ID'
+            });
+        }
+        const {email, role, ...campos} = req.body;
+
+        if(userUpdate.email !== email){
+            const existeEmail = await User.find({email});
+            if(existeEmail){
+                return res.status(400).json({
+                    ok:true,
+                    message: 'Ya existe un usuario con ese email'
+                });
+            }
+        }
+        
+        //TODO: MODIFICAR ROLES DE USUARIO 
+
+        console.log(campos);
+        const userUpdated = await User.findByIdAndUpdate(id, campos, {new:true});
+
+        res.status(200).json({
+            ok:true,
+            message: 'Usuario actualizado',
+            user: userUpdated
+        });
+
+    } catch (error) {
+        console.error('ERROR ',error);
+        
+        res.status(500).json({
+            ok:false,
+            message: 'Error inesperado..revisa logs'
+        })
+    }
+}
+
+userController.deleteUser = async(req, res)=>{
+    const id = req.params.user_id;
+    try {
+        const user = await User.findById(id);
+        if(!user){
+            return res.status(404).json({
+                ok:false,
+                message: 'No existe un usuario con ese ID'
+            });
+        }
+
+        await User.findByIdAndDelete(id);
+
+        res.json({
+            ok:true,
+            message: 'Usuario eliminado'
+        });
+    } catch (error) {
+        
     }
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,12 +4,17 @@ const { validarCampos } = require('../middlewares/validarCampos');
 
 const router = Router();
 
-const {getUsers, getUserById, createUser} = require('../controllers/user.controller');
+const {getUsers, getUserById, createUser, updateUser, deleteUser} = require('../controllers/user.controller');
 
+//TODO: Validar token
+
+//Ruta: Obtener todos los usuarios
 router.get('/', getUsers);
 
+//Ruta: Obtener un usuario por ID
 router.get('/:user_id', getUserById);
 
+//Ruta: Crear un usuario
 router.post('/', 
     [
         check('first_name', 'El nombre es obligatorio').not().isEmpty(),
@@ -21,5 +26,19 @@ router.post('/',
     createUser
 );
 
+//Ruta: Editar un usuario
+router.put('/:user_id',
+    [
+        check('first_name', 'El nombre es obligatorio').not().isEmpty(),
+        check('last_name', 'El apellido es obligatorio').not().isEmpty(),
+        check('email', 'El correo es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        check('role', 'El rol de usuario es obligatorio').not().isEmpty(),
+        validarCampos,
+    ], 
+    updateUser);
+
+//Ruta: eliminar un usuario
+router.delete('/:user_id', deleteUser);
 
 module.exports = router;
