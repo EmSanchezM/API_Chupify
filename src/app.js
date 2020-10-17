c68caa9f371965aada86eac96c2c2ee47f0207a4
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-10-17 14:17:42 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const createRoles = require('./helpers/initSetup')
 
@@ -13,6 +14,7 @@ createRoles();
 
 app.set("port", process.env.PORT || 4000);
 app.use(helmet());
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
